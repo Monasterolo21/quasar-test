0429837e401d8160831ddf0aa58df95c3c9691de
Samuele Monasterolo <smonasterolo2@gmail.com>
2022-05-01 20:52:06 +0200

    init

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,50 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-row>
+      <q-col>
+        <p class="content">
+          {{ currentFact }}
+        </p>
+      </q-col>
+    </q-row>
+
+    <q-row>
+      <q-col>
+        <q-btn @click="fetchFact()" label="Get Fact" color="primary" flat />
+      </q-col>
+    </q-row>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+
+  data() {
+    return {
+      currentFact: "",
+    };
+  },
+
+  methods: {
+    async fetchFact() {
+      const response = await fetch("https://catfact.ninja/fact");
+      const data = await response.json();
+      this.currentFact = data.fact;
+    },
+  },
+
+  mounted() {
+    this.fetchFact();
+  },
+});
+</script>
+
+<style scoped>
+.content {
+  font-size: 1.5em;
+  padding: 2em;
+}
+</style>
